fa697506413f366a9ebc41649d67765dfdb8f81f
gabyafm <gabyafmls@gmail.com>
2024-01-31 14:52:06 -0400

    configurações

---
+++ .storybook/main.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../dist/stories/**/*.stories.{js,md,mdx}'],
+  addons: ['storybook-addon-themes'],
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+export const parameters = {
+  themes: {
+    default: 'vivo',
+    list: [
+      { name: 'vivo', class: 'vivo', color: '#00aced' },
+      { name: 'telefonica', class: 'theme-tf', color: '#3b5998' },
+      { name: 'ssss', class: 'theme-tfcs', color: '#3b5998' },
+    ],
+  },
+};
