9914567527efa19aca2c02283ed8ea222b9e5e64
igortargino <igortargino01@gmail.com>
2024-02-05 18:40:39 -0300

    feat: add theme provider web components

--- tools/theme-provider.ts
+++ tools/theme-provider.ts
@@ -1,43 +1,59 @@
-import { LitElement, html, css } from 'lit';
-import { createContext, provide,  } from '@lit/context';
-import { loadJson } from './load-css-tokens';
-import DesignSystem from './types/design-system';
+import { LitElement, css, html } from 'lit';
+import { loadCssTokens } from './load-css-tokens';
 import { DesignSystemKeys } from './types/design-system-keys';
-import { customElement } from 'lit/decorators.js';
-
-const themeContext = createContext<DesignSystem | undefined>('theme');
+import DesignSystem from './types/design-system';
+import { customElement, property } from 'lit/decorators.js';
+import Theme from './types/theme';
+import Palette from './types/palette';
 
 @customElement('theme-provider')
-class ThemeProvider extends LitElement {
-  @provide({ context: themeContext })
-  theme: DesignSystem | undefined;
+export class ThemeProvider extends LitElement {
+  @property({ type: Object })
+  designSystem!: DesignSystem;
 
+  @property({ type: Object })
+  theme!: Theme;
+
+  @property({ type: Object })
+  palette!: Palette;
+
+  override async connectedCallback() {
+    super.connectedCallback();
+    this.designSystem = await loadCssTokens(DesignSystemKeys.Movistar);
+    const { global, ...theme } = this.designSystem;
+    this.theme = theme;
+    this.palette = global.palette;
+
+    for (const key in this.theme.dark) {
+      const match = this.theme.dark[key].value.match(/\{palette\.(\w+)\}/);
+      if (match) {
+        this.theme.dark[key].value = this.palette[match[1]].value;
+      }
+    }
+
+    for (const key in this.theme.light) {
+      const match = this.theme.light[key].value.match(/\{palette\.(\w+)\}/);
+      if (match) {
+        this.theme.light[key].value = this.palette[match[1]].value;
+      }
+    }
+  }
+
+
+  changeDesignSystem(newDesignSystem: DesignSystem) {
+    this.designSystem = newDesignSystem;
+    this.requestUpdate();
+  }
 
   static override styles = css`
-    :host {
-      display: contents;
-    }
+    /* Use the theme to generate CSS */
   `;
-
-  constructor() {
-    super();
-    this.loadTheme();
-  }
-
-  async loadTheme() {
-    this.theme = await loadJson(DesignSystemKeys.Telefonica);
-    console.log(this.theme)
-  }
 
   override render() {
     return html`
-      <slot>
-        <h1>
-          ${this.theme}
-        </h1>
-      </slot>
+      <style>
+      </style>
+      <slot></slot>
     `;
   }
 }
-
-export { themeContext, ThemeProvider }
