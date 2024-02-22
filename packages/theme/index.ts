b883599b0ae6c43c41893c3ee092ead899c09b73
igortargino <igortargino01@gmail.com>
2024-02-22 15:51:44 -0300

    fix: remove unusable function

--- packages/theme/index.ts
+++ packages/theme/index.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { Theme, ThemeType } from '../../tools/theme/types/theme';
 import { TokenType } from '../../tools/theme/types/token';
