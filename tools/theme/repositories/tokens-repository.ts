db9a514a13001bc3498553cb2b320010b7286963
igortargino <igortargino01@gmail.com>
2024-02-23 21:10:27 -0300

    fix: palette colors rgba

--- tools/theme/repositories/tokens-repository.ts
+++ tools/theme/repositories/tokens-repository.ts
@@ -44,13 +44,38 @@ class TokensRepository {
 
     ['dark', 'light'].forEach((themeType) => {
       for (const key in newTokens[themeType]) {
-        newTokens[themeType][key].value = newTokens[themeType][key].value.replace(/\{palette\.(\w+)\}/g, (match: string, p1: string) => {
-          return tokens.global.palette[p1]?.value || match;
-        });
+        if (newTokens[themeType][key].value.includes('rgba')) {
+          newTokens[themeType][key].value = this.replaceRgbaPaletteValues(newTokens[themeType][key].value, tokens);
+        } else {
+          newTokens[themeType][key].value = this.replaceNormalPaletteValues(newTokens[themeType][key].value, tokens);
+        }
       }
     });
 
     return newTokens;
+  }
+
+  private replaceNormalPaletteValues(value: string, tokens: Token): string {
+    return value.replace(/\{palette\.(\w+)\}/g, (match: string, p1: string) => {
+      const paletteValue = tokens.global.palette[p1]?.value;
+      return paletteValue || match;
+    });
+  }
+
+  private replaceRgbaPaletteValues(value: string, tokens: Token): string {
+    return value.replace(/rgba\(\{palette\.(\w+)\}, (\d+(?:\.\d+)?)\)/g, (match: string, paletteKey: string, opacity: string) => {
+      const paletteValue = tokens.global.palette[paletteKey]?.value;
+      return paletteValue && paletteValue.startsWith('#') ? this.hexToRgbA(paletteValue, parseFloat(opacity)) : match;
+    });
+  }
+
+  private hexToRgbA(hex: string, opacity: number): string {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if (!result) {
+      throw new Error(`Invalid hex color: ${hex}`);
+    }
+    const [r, g, b] = result.slice(1).map(hex => parseInt(hex, 16));
+    return `rgba(${r},${g},${b},${opacity})`;
   }
 }
 
