5bdfccf08ca874a4d8e87653ee5d31bbbb4bac51
gabyafm <gabyafmls@gmail.com>
2024-02-26 13:11:47 -0400

    fix: update text-component index file

--- packages/text/index.ts
+++ packages/text/index.ts
@@ -1,116 +1,107 @@
-import { html, css, PropertyValueMap } from 'lit';
+import { html, css, PropertyValueMap, CSSResultGroup } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { textPresets } from './presets';
 import { ThemeWebComponent } from '../theme';
+
+type Tag = 'h1' | 'h2' | 'h3' | 'p' | 'span'
+type FontWeight = 'light' | 'regular' | 'medium' | 'bold'
+type Preset = 'text1' | 'text2' | 'text3' | 'text4' | 'text5' | 'text6' | 'text7' | 'text8' | 'text9' | 'text10'
+
 
 @customElement('text-web-component')
 export class TextWebComponent extends ThemeWebComponent {
-  static override styles = css`
-    :host {
-      display: inline-block;
-    }
-    .text {
-      color: var(--text-color, red);
-      text-decoration: var(--text-decoration, none);
-      text-transform: var(--text-transform, none);
-      overflow: hidden;
-      text-overflow: var(--text-truncate, initial);
-      font-size: var(--text-size, 14px);
-      font-weight: var(--text-weight, 400)
-    }
-  `;
+  static override get styles(): CSSResultGroup {
+    return [
+      super.styles,
+      css`
+      :host {
+        font-size: var(--text-size-mobile);
+        font-weight: var(--text-weight);
+        line-height: var(--text-line-height-mobile);
+        margin: 0;
+        overflow-wrap: anywhere;
+        word-break: break-word;
+        line-clamp: var(--truncate-lines, 1);
+      }
 
-  @property({ type: String }) decoration = 'none';
-  @property({ type: String }) transform = 'none';
-  @property({ type: String }) truncate = 'initial';
-  @property({ type: Boolean }) forceMobileSizes = false;
-  @property({ type: String }) textShadow?: string;
-  @property({ type: Object }) dataAttributes?: Record<string, string>;
-  @property({ type: String }) textType?: 'text1' | 'text2' | 'text3' | 'text4' | 'text5' | 'text6' | 'text7' | 'text8' | 'text9' | 'text10';
-  @property({ type: Boolean, reflect: true }) medium = false;
-  @property({ type: Boolean, reflect: true }) regular = false;
-  @property({ type: Boolean, reflect: true }) light = false;
-  @property({ type: Boolean, reflect: true }) bold = false;
+      :host(:not([wordBreak])) {
+        overflow-wrap: inherit;
+        word-break: inherit;
+      }
 
+      :host([truncate='1']) {
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        word-break: break-all;
+      }
+
+      :host([truncate]) {
+        display: -webkit-box;
+        overflow: hidden;
+        -webkit-line-clamp: var(--truncate-lines);
+        -webkit-box-orient: vertical;
+      }
+
+      @media (min-width: 768px) {
+        :host {
+          font-size: var(--text-size-desktop);
+          line-height: var(--text-line-height-desktop);
+        }
+      }
+    `
+    ];
+
+  }
+
+  @property({ type: String }) preset: Preset = 'text1';
+  @property({ type: String }) as: Tag = 'span';
+  @property({ type: String, reflect: true }) weight: FontWeight = 'regular'
+  @property({ type: Number, reflect: true }) truncate: number | null = null;
+  @property({ type: Boolean, reflect: true }) wordBreak = true;
 
   protected override updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
     super.updated(changedProperties);
-    if (changedProperties.has("_theme")) {
+
+    if (changedProperties.has('_theme') || changedProperties.has('truncate') || changedProperties.has('preset')) {
       this.updateStyles();
     }
-    console.log(this, 'this')
-    this.updateFontWeight();
+    if (changedProperties.has('weight') || changedProperties.has('_theme')) {
+      this.setFontWeight();
+    }
+  }
+
+  private setFontWeight() {
+    const validTextTypes = ['text5', 'text6', 'text7', 'text8', 'text9', 'text10'];
+    if (validTextTypes.includes(this.preset)) {
+
+      const typeWeight = this.theme?.text?.weight?.[this.preset as keyof typeof this.theme.text.weight]?.value;
+      this.weight = typeWeight as FontWeight;
+      let fontWeightVarName = `--font-weight-${this.weight}`;
+      let fontWeightValue = getComputedStyle(this).getPropertyValue(fontWeightVarName).trim();
+      this.style.setProperty('--text-weight', `${fontWeightValue}`);
+    }
   }
 
   private updateStyles() {
-    const sizes = {
-      text1: { size: '12px', lineHeight: '16px' },
-      text2: { size: '14px', lineHeight: '20px' },
-      text3: { size: '16px', lineHeight: '24px' },
-      text4: { size: '18px', lineHeight: '24px' },
-      text5: { size: '20px', lineHeight: '24px' },
-      text6: { size: '24px', lineHeight: '32px' },
-      text7: { size: '28px', lineHeight: '32px' },
-      text8: { size: '32px', lineHeight: '40px' },
-      text9: { size: '40px', lineHeight: '48px' },
-      text10: { size: '48px', lineHeight: '56px' },
-    };
-    const type = this.textType && sizes[this.textType] ? this.textType : 'text2';
-    const { size, lineHeight } = sizes[type];
-    this.style.setProperty('--text-size', size);
-    this.style.setProperty('--text-line-height', lineHeight);
-    if (this.theme) {
-      this.style.setProperty('--text-color', this.theme.componentsColor.textPrimary.value);
-    }
+    const preset = textPresets[this.preset];
+    this.style.setProperty('--text-size-mobile', `${preset.mobileSize}px`);
+    this.style.setProperty('--text-size-desktop', `${preset.desktopSize}px`);
+    this.style.setProperty('--text-line-height-mobile', preset.mobileLineHeight);
+    this.style.setProperty('--text-line-height-desktop', preset.desktopLineHeight);
+    this.style.setProperty('--truncate-lines', this.truncate ? String(this.truncate) : this.style.removeProperty('--truncate-lines'));
   }
 
-  private updateFontWeight() {
-    const mapToWeight = {
-      light: '300',
-      regular: '400',
-      medium: '500',
-      bold: '700',
-    };
-
-    const defaultWeight = '400';
-    let weight;
-
-    if (this.light || this.medium || this.regular || this.bold) {
-      weight = this.light ? mapToWeight.light :
-        this.medium ? mapToWeight.medium :
-          this.bold ? mapToWeight.bold :
-            mapToWeight.regular;
-    } else {
-
-      const validTextTypes = ['text5', 'text6', 'text7', 'text8', 'text9', 'text10'];
-
-      if (this.textType && validTextTypes.includes(this.textType)) {
-        const typeWeight = this.theme?.text?.weight?.[this.textType as keyof typeof this.theme.text.weight].value;
-        weight = typeWeight ? typeWeight : defaultWeight;
-      } else {
-        weight = defaultWeight;
-      }
-    }
-
-    this.style.setProperty('--text-weight', weight);
-  }
   override render() {
-    const classes: { [key: string]: boolean } = {
-      'text-mobile-sizes': this.forceMobileSizes,
+    const tagRenderers = {
+      'h1': html`<h1><slot></slot></h1>`,
+      'h2': html`<h2><slot></slot></h2>`,
+      'h3': html`<h2><slot></slot></h2>`,
+      'p': html`<p><slot></slot></p>`,
+      'span': html`<span><slot></slot></span>`,
     };
 
-    const styles: { [key: string]: string | undefined } = {
-      'text-shadow': this.textShadow,
-    };
-
-    return html`
-      <span
-        class="text ${Object.keys(classes).filter(key => classes[key]).join(' ')}"
-        style="${Object.keys(styles).map(key => `${key}: ${styles[key]};`).join(' ')}"
-        id="${this.id}"
-        ?data-attributes="${!!this.dataAttributes}"
-      >
-        <slot></slot>
-      </span>
-    `;
+    return tagRenderers[this.as] || tagRenderers['span'];
   }
 }
+
