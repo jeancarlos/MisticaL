fd32866e9af1447408e0ea22fc4cd1d9c99797ab
gabyafm <gabyafmls@gmail.com>
2024-02-27 15:42:51 -0400

    chore: reorganize project directory structure

--- packages/theme/index.ts
+++ packages/theme/index.ts
@@ -14,22 +14,22 @@ export class ThemeWebComponent extends LitElement {
     return css`
     @font-face {
     font-family: 'VivoType';
-    src: url('./fonts/WOFF2/VivoTypeW05-Light.woff2') format('woff2'),
-     url('./fonts/WOFF/VivoTypeW05-Light.woff') format('woff');
+    src: url('../../tools/theme/fonts/WOFF2/VivoTypeW05-Light.woff2') format('woff2'),
+     url('../../tools/theme/fonts/WOFF/VivoTypeW05-Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal
+  }
+    @font-face {
+    font-family: 'VivoType';
+    src: url('../../tools/theme/fonts/WOFF2/VivoTypeW05-Regular.woff2') format('woff2'),
+     url('../../tools/theme/fonts/WOFF/VivoTypeW05-Regular.woff') format('woff');
     font-weight: 400;
     font-style: normal
   }
     @font-face {
     font-family: 'VivoType';
-    src: url('./fonts/WOFF2/VivoTypeW05-Regular.woff2') format('woff2'),
-     url('./fonts/WOFF/VivoTypeW05-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal
-  }
-    @font-face {
-    font-family: 'VivoType';
-    src: url('./fonts/WOFF2/VivoTypeW05-Bold.woff2') format('woff2'),
-     url('./fonts/WOFF/VivoTypeW05-Bold.woff') format('woff');
+    src: url('../../tools/theme/fonts/WOFF2/VivoTypeW05-Bold.woff2') format('woff2'),
+     url('../../tools/theme/fonts/WOFF/VivoTypeW05-Bold.woff') format('woff');
       font-weight: 700;
       font-style: normal
   }
